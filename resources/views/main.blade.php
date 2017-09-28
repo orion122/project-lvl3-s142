18a8e27a4668c8a48a315ef83da775b17404e95d
ramazan <yaru@bk.ru>
2017-09-28 11:05:18 +0300

    remove class='active' from Home link

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -22,7 +22,7 @@
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="{{ route('index') }}">Home<span class="sr-only">(current)</span></a></li>
+                        <li><a href="{{ route('index') }}">Home<span class="sr-only">(current)</span></a></li>
                         <li><a href="{{ route('domains') }}">Domains</a></li>
                     </ul>
 
