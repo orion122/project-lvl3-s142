bd521e739043a165447336a4af1462f8403bcf55
ramazan <yaru@bk.ru>
2017-09-29 19:49:16 +0300

    add CEO

--- resources/views/id.blade.php
+++ resources/views/id.blade.php
@@ -9,6 +9,9 @@
             <th>URL</th>
             <th>Status Code</th>
             <th>Content Length</th>
+            <th>h1</th>
+            <th>Meta Keywords</th>
+            <th>Meta Content</th>
             <th>Created At</th>
             <th>Updated At</th>
         </tr>
@@ -22,6 +25,9 @@
             <td>{{ $row->name }}</td>
             <td>{{ $row->status_code }}</td>
             <td>{{ $row->content_length }}</td>
+            <td>{{ $row->h1 }}</td>
+            <td>{{ $row->meta_keywords }}</td>
+            <td>{{ $row->meta_content }}</td>
             <td>{{ $row->created_at }}</td>
             <td>{{ $row->updated_at }}</td>
         </tr>
