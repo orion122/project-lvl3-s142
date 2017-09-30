94520aa487b9096de76a331e30080bcd9c707f63
ramazan <yaru@bk.ru>
2017-09-30 20:50:56 +0300

    edit test

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -1,54 +1,56 @@
 <?php
-//
-//use Laravel\Lumen\Testing\DatabaseMigrations;
-//use Laravel\Lumen\Testing\DatabaseTransactions;
-//use Illuminate\Support\Facades\DB;
-//use GuzzleHttp\Client;
-//
-//class DomainsPageTest extends TestCase
-//{
-//    /**
-//     * A basic test example.
-//     *
-//     * @return void
-//     */
-//    public function testExample()
-//    {
-//        $url = 'http://ya.ru';
-////        $client = new Client(['base_uri' => $url]);
-////        $response = $client->request('GET');
-////        $contentLengthArray = $response->getHeader('Content-Length');
-////        $contentLength = !empty($contentLengthArray) ? $contentLengthArray[0] : null;
-////        $statusCode = $response->getStatusCode();
-////        $body = (string) $response->getBody();
-////        $time = Carbon\Carbon::now();
-//
-//        $statusCode = '200';
-//        $contentLength = '1';
-//        $body = 'a';
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client;
+
+class DomainsPageTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $url = 'http://ya.ru';
+//        $client = new Client(['base_uri' => $url]);
+//        $response = $client->request('GET');
+//        $contentLengthArray = $response->getHeader('Content-Length');
+//        $contentLength = !empty($contentLengthArray) ? $contentLengthArray[0] : null;
+//        $statusCode = $response->getStatusCode();
+//        $body = (string) $response->getBody();
 //        $time = Carbon\Carbon::now();
-//
-//        $id = DB::table('domains')->insertGetId([
-//            'name' => $url,
-//            'status_code' => $statusCode,
-//            'content_length' => $contentLength,
-//            'body' => $body,
-//            'created_at' => $time
-//        ]);
-//
+
+        $statusCode = '200';
+        $contentLength = '1';
+        $body = 'a';
+        $time = Carbon\Carbon::now();
+
+        $id = DB::table('domains')->insertGetId([
+            'name' => $url,
+            'status_code' => $statusCode,
+            'content_length' => $contentLength,
+            'body' => $body,
+            'created_at' => $time
+        ]);
+
 //        $page = intval($id / 5);
 //
 //        $this->get("/domains?page=$page");
-//
-//        /*$list = "<td>$id</td>
-//                        <td><a href=\"domains/$id\">$url</a></td>
-//                        <td>$statusCode</td>
-//                        <td>$contentLength</td>
-//                        <td>$time</td>
-//                        <td></td>";
-//
-//        $this->assertContains($list, $this->response->getContent());*/
-//
-//        $this->assertResponseOk();
-//    }
-//}
+
+        $this->get("/domains");
+
+        /*$list = "<td>$id</td>
+                        <td><a href=\"domains/$id\">$url</a></td>
+                        <td>$statusCode</td>
+                        <td>$contentLength</td>
+                        <td>$time</td>
+                        <td></td>";
+
+        $this->assertContains($list, $this->response->getContent());*/
+
+        $this->assertResponseOk();
+    }
+}
