f5a7f85acc2a40ffed2239725011e18618217ee1
ramazan <yaru@bk.ru>
2017-10-10 09:35:58 +0300

    add description and add button value

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,14 +5,15 @@
     <div class="container">
         <div class="jumbotron">
             <div class="container">
-                <h1>Web page Analyzer</h1>
+                <h1>Web Page Analyzer</h1>
+                <p>Check web pages for free</p>
                 <form action="{{ route('store') }}" method="POST">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="input-group">
                                 <input type="text" name="url" required class="form-control" placeholder="https://example.com">
                                 <span class="input-group-btn">
-                                        <input class="btn btn-default" type="submit">
+                                        <input class="btn btn-default" type="submit" value="Check">
                                     </span>
                             </div><!-- /input-group -->
                         </div><!-- /.col-lg-6 -->
